add23bd02f818495e6c7a8507cf07280110854d5
Eric Lekwa <lekwaeric@yahoo.com>
2020-02-12 19:44:05 +0100

    Initial Commit

---
+++ app/src/main/java/tech/mpanaka/test/ui/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package tech.mpanaka.test.ui.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.mpanaka.test.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
